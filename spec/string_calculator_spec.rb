9ca4bad73a5ff561bdaa586a099f6be44d30091d
Harsh <hsagar63@gmail.com>
2024-08-29 12:44:24 +0530

    added test for comma separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,9 @@ RSpec.describe StringCalculator do
     it "returns the same number when only one input is given" do
       expect(described_class.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers" do
+      expect(described_class.add("1,2")).to eq(3)
+    end
   end
 end
