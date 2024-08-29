635eb774fda4ba05358153ad65e679af0b88b1de
Harsh <hsagar63@gmail.com>
2024-08-29 11:34:07 +0530

    added test for the 'add' method existence

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator do
   it "defines the StringCalculator class" do
     expect(Object.const_defined?("#{described_class}")).to be true
   end
+
+  it "has the add method" do
+    expect(described_class).to respond_to(:add)
+  end
 end
